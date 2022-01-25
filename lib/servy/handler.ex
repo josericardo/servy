d2a66bcacc34b2b8616b99f72a4b9672a72209ce
José Ricardo <josericardo.dasilva@britecore.com>
2022-01-25 08:20:20 -0400

    First commit, project scaffold and basic request processing

---
+++ lib/servy/handler.ex
@@ -0,0 +1,152 @@
+defmodule Servy.Handler do
+  def handle(request) do
+    request
+    |> parse
+    |> log
+    |> rewrite_path
+    |> route
+    |> track
+    |> format_response
+  end
+
+  def log(conv), do: IO.inspect conv
+
+  def parse(request) do
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first
+      |> String.split(" ")
+
+    %{ method: method, status: nil, path: path, resp_body: "" }
+  end
+
+  def rewrite_path(%{ path: "/wildlife" } = conv) do
+    %{ conv | path: "/wildthings" }
+  end
+
+  def rewrite_path(%{ path: "/bears?id=" <> bear_id } = conv) do
+    %{ conv | path: "/bears/#{bear_id}" }
+  end
+
+  def rewrite_path(conv), do: conv
+
+  def track(%{ status: 404 } = conv) do
+    IO.puts("404 for #{conv.path}")
+    conv
+  end
+
+  def track(conv), do: conv
+
+  def route(conv) do
+    route(conv, conv.method, conv.path)
+  end
+
+  def route(conv, "GET", "/wildthings") do
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
+  end
+
+  def route(conv, "GET", "/bears" <> id) do
+    %{ conv | status: 200, resp_body: "bear #{id}" }
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{ conv | status: 200, resp_body: "Teddy, Paddington" }
+  end
+
+  def route(conv, "GET", "/bigfoot") do
+    %{ conv | status: 200, resp_body: "roooarrr" }
+  end
+
+  def route(conv, "DELETE", "/bears/" <> _id) do
+    %{ conv | status: 403, resp_body: "Deleting a bear is forbidden!" }
+  end
+
+  def route(conv, _method, path) do
+    %{ conv | status: 404, resp_body: "No #{path} here!" }
+  end
+
+
+  def format_response(conv) do
+    """
+    HTTP/1.1 #{conv.status} #{status_msg(conv.status)}
+    Content-Type: text/html
+    Content-Length: #{byte_size(conv.resp_body)}
+
+    #{conv.resp_body}
+    """
+  end
+
+  defp status_msg(status_code) do
+    %{
+      200 => "OK",
+      404 => "NOT FOUND"
+    }[status_code]
+  end
+end
+
+request = """
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /bears/2 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+DELETE /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts(response)
