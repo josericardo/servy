52d9e8d73483df32f4132266d4dd39712532b1ee
José Ricardo <josericardo.dasilva@britecore.com>
2022-01-29 15:46:35 -0400

    Replacing remaining ad-hoc inspections with unit tests

--- test/servy_test.exs
+++ test/servy_test.exs
@@ -30,4 +30,36 @@ defmodule ServyHandlerTest do
 
     assert Servy.Handler.rewrite_path(conv).path == "/wildthings"
   end
+
+  test "Handles requests" do
+    request = """
+    GET /bears HTTP/1.1
+    Host: example.com
+    User-Agent: ExampleBrowser/1.0
+    Accept: */*
+
+    """
+
+    expected = """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: 15
+
+    😜 bear  😎
+    """
+
+    assert Servy.Handler.handle(request) == expected
+  end
+
+  test "Handles requests for individual bears" do
+    request = """
+    GET /bears/2 HTTP/1.1
+    Host: example.com
+    User-Agent: ExampleBrowser/1.0
+    Accept: */*
+
+    """
+    response = Servy.Handler.handle(request)
+    assert String.contains?(response, "bear /2")
+  end
 end
